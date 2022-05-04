515cf74bf0cef17574de23193eab58978996795f
Álvaro Gil González <104679283+AlvaroGilGonzalez@users.noreply.github.com>
2022-05-04 17:50:32 +0200

    Add files via upload

---
+++ GilAlvaroBaloncestoV2/src/Persona.java
@@ -0,0 +1,31 @@
+
+public class Persona {
+	
+	//Atributos del objeto
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	
+	//Metodo constructor por parametro
+	public Persona(String nombre, String apellidos, String dni) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+	}
+	
+	//Metodo que nos devuelve un String con la informacion de la persona
+	@Override
+	public String toString() {
+		return "nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni;
+	}
+	
+	//Metodo getter para el nombre de la persona
+	public String getNombre() {
+		return nombre;
+	}
+
+	
+	
+	
+
+}
